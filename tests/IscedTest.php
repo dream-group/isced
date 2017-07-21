dbd6d2c808596f941dc4c3a7e10f94f3be30f95a
Safak Samiloglu <safak.samiloglu@dreamapply.com>
2017-07-21 10:55:56 +0300

    More progres.

--- tests/IscedTest.php
+++ tests/IscedTest.php
@@ -31,9 +31,38 @@ class IscedTest extends PHPUnit_Framework_TestCase
 	
     public function testCrumbsByCode()
     {
+        $this->assertEquals(Isced::fetchCrumbsByCode('01015', Isced::VERSION_2011), [
+            '01015' => 'Religion/Ethics',
+            '010'   => 'Basic/broad, general programmes',
+            '01'    => 'Basic/broad, general programmes',
+            '0'     => 'General Programmes'
+        ]);
+        $this->assertEquals(Isced::fetchCrumbsByCode('1422', Isced::VERSION_2011), [
+            '1422'  => 'Education science 05.7',
+            '142'   => 'Education science',
+            '14'    => 'Teacher training and education science',
+            '1'     => 'Education'
+        ]);
+        $this->assertEquals(Isced::fetchCrumbsByCode('7', Isced::VERSION_2011), [
+            '7'     => 'Health and Welfare'
+        ]);
 
+        $this->assertEquals(Isced::fetchCrumbsByCode('0113', Isced::VERSION_2013), [
+            '0113'  => 'Teacher training without subject specialization',
+            '011'   => 'Education 011',
+            '01'    => 'Education'
+        ]);
+        $this->assertEquals(Isced::fetchCrumbsByCode('061', Isced::VERSION_2013), [
+            '061'   => 'Information and Communication Technologies (ICTs)',
+            '06'    => 'Information and Communication Technologies (ICTs)'
+        ]);
+        $this->assertEquals(Isced::fetchCrumbsByCode('0917', Isced::VERSION_2013), [
+            '0917'  => 'Traditional and complementary medicine and therapy',
+            '091'   => 'Health',
+            '09'    => 'Health and welfare'
+        ]);
     }
-	
+
     public function testFullList()
     {
 

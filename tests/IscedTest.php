e9e4fbb167f31e71fbc3aaf5f93be4e486e85832
Safak Samiloglu <safak.samiloglu@dreamapply.com>
2017-07-25 15:45:34 +0300

    Trying tests again.

--- tests/IscedTest.php
+++ tests/IscedTest.php
@@ -2,6 +2,11 @@
 
 use Dream\Isced;
 
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 class IscedTest extends PHPUnit_Framework_TestCase
 {
 	
